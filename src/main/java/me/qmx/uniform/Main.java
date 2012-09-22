f361a9d7dafdb8e910b263fac489c686c365823c
Douglas Campos <qmx@qmx.me>
2012-09-22 00:30:23 -0300

    basic cmdline parser + entry point

--- src/main/java/me/qmx/uniform/Main.java
+++ src/main/java/me/qmx/uniform/Main.java
@@ -1,7 +1,33 @@
 package me.qmx.uniform;
 
-public class Main {
-    public static void main(String[] args) {
+import org.kohsuke.args4j.Argument;
+import org.kohsuke.args4j.CmdLineException;
+import org.kohsuke.args4j.CmdLineParser;
+import org.kohsuke.args4j.Option;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    @Option(name = "-f", usage = "eclipse formatting settings")
+    private File configFile;
+
+    @Argument
+    private List<File> files = new ArrayList<File>();
+
+    public static void main(String[] args) {
+        new Main().run(args);
+    }
+
+    private void run(String[] args) {
+        final CmdLineParser parser = new CmdLineParser(this);
+        try {
+            parser.parseArgument(args);
+            new Uniform(configFile).format(files);
+        } catch (CmdLineException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
     }
 }
---
+++ src/main/java/me/qmx/uniform/Uniform.java
@@ -0,0 +1,13 @@
+package me.qmx.uniform;
+
+import java.io.File;
+import java.util.List;
+
+public class Uniform {
+    public Uniform(File configFile) {
+    }
+
+    public void format(List<File> files) {
+        //To change body of created methods use File | Settings | File Templates.
+    }
+}
